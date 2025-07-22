02b630b6f8747b841655f86447d5c7e8b8cdee7f
AFranklyn <akanofranklyn@gmail.com>
2025-07-22 13:27:03 +0100

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-4xl text-center'>
-      Welcome To Iron Temple!
+      Welcome To Iron Temple, a home of Iron, muscles and Gains!
     </div>
   )
 }
